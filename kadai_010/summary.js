2259f95f9bb5dba8e523da1a73bca00d0972e8ba
YK423a <mwdjg0@gmail.com>
2025-05-22 21:34:08 +0900

    Add files via upload

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -1,17 +1,17 @@
 $(function() {
     $('#change-color').on('click', function() {
-        $('#target').css('color',red);        
+        $('#target').css('color',"red");        
     });
 
-    $('change-text').on('click', function() {
+    $('#change-text').on('click', function() {
         $('#target').text('Hello');
     });
 
-    $('fade-out').on('click', function() {
-        $('#target').css('opacity',0);        
+    $('#fade-out').on('click', function() {
+        $('#target').fadeOut();        
     });
 
-    $('fade-in').on('click', function() {
-        $('#target').css('opacity' , 1);
+    $('#fade-in').on('click', function() {
+        $('#target').fadeIn();
     });
 });
